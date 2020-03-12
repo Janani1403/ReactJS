08c9d1db03c51beebca5dafc9979d97c6ef3457f
Janani1403 <janani.haran1403@gmail.com>
2020-03-12 14:27:15 +0530

    Components Part 1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Menu from './components/MenuComponent';
+<Menu />
 import { Navbar, NavbarBrand } from 'reactstrap';
 
 class App extends Component {
